2c61b62b8733d4bd0f01a0f5c7f13406df9b2e90
Jamie Near <jamie.near@mcgill.ca>
2015-01-12 12:54:42 -0500

    Fixed bug in readlcmcoord_getBackground.m

--- processingTools/readlcmcoord_getBackground.m
+++ processingTools/readlcmcoord_getBackground.m
@@ -24,7 +24,7 @@ line=fgets(fid)
 ppmstr='points on ppm-axis ='
 ppm_index=findstr(line,ppmstr);
 while isempty(ppm_index)
-    line=fgets(fid)
+    line=fgets(fid);
     ppm_index=findstr(line,ppmstr);
 end
 
@@ -50,20 +50,20 @@ switch part
     case 'sp'
         bgstr='NY phased data points follow'
     case 'fit'
-        bgstr='NY points of the fit to the follow'
+        bgstr='NY points of the fit to the data follow'
     otherwise
         error('ERROR:  part not found');
 end
 bg_index=findstr(line,bgstr);
 while isempty(bg_index)
-    line=fgets(fid)
+    line=fgets(fid);
     bg_index=findstr(line,bgstr);
 end
 
 line=fgets(fid);
 while linenum<=(Npts/10)
     [A,count, errmsg, nextindex] = sscanf(line, '%f', inf);
-    A
+    A;
     bg((linenum-1)*10+1:linenum*10,1)=A;
     linenum=linenum+1;
     line=fgets(fid);
