4e46f074694b652ab3bfb91adfe36a70100a3f6e
jamienear <jamie.near@mcgill.ca>
2015-07-29 15:21:59 -0400

    Added a new function for extracting a specific time range from the FID of a dataset.  The result is similar to op_zerotrim.m, except that op_timerange can trim both leading points and trailing points from the fid.

---
+++ processingTools/op_timerange.m
@@ -0,0 +1,52 @@
+% op_timerange.m
+% Jamie Near, McGill University 2014.
+% 
+% USAGE:
+% out=op_timerange(in,ppmmin,ppmmax);
+% 
+% DESCRIPTION:
+% Output only a specified frequency range of the input spectrum.
+% 
+% INPUTS:
+% in         = input data in matlab structure format.
+% ppmmin     = minimum extent of frequency range in ppm.
+% ppmmax     = maximum extent of frequency range in ppm.
+
+function out=op_timerange(in,tmin,tmax);
+
+%Take only the specified range of the FID
+fids=in.fids(in.t>=tmin & in.t<tmax,:,:);
+
+%Calculate Specs using fft
+specs=fftshift(ifft(fids,[],in.dims.t),in.dims.t);
+
+%calculate the size;
+sz=size(fids);
+
+%calculate the time scale
+t=in.t(in.t>tmin & in.t<tmax);
+
+%Now re-calculate t and ppm arrays using the calculated parameters:
+f=[(-in.spectralwidth/2)+(in.spectralwidth/(2*sz(1))):...
+    in.spectralwidth/(sz(1)):...
+    (in.spectralwidth/2)-(in.spectralwidth/(2*sz(1)))];
+
+ppm=-f/(in.Bo*42.577);
+ppm=ppm+4.6082;
+
+%calculate the time scale
+t=[0:in.dwelltime:(sz(1)-1)*in.dwelltime];
+
+
+
+%FILLING IN DATA STRUCTURE
+out=in;
+out.fids=fids;
+out.specs=specs;
+out.sz=sz;
+out.ppm=ppm;  
+out.t=t; 
+
+%FILLING IN THE FLAGS
+out.flags=in.flags;
+out.flags.writtentostruct=1;
