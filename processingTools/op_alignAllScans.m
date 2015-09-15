395d928725149839552332c4049e6120ce4fd6c9
jamienear <jamie.near@mcgill.ca>
2015-09-15 13:22:37 -0400

    Added new functions op_alignAllScans.m and op_alignAllScans_fd.m for aligning a large group of scans to eachother (since the op_alignScans.m function can only align two spectra at at time).

---
+++ processingTools/op_alignAllScans.m
@@ -0,0 +1,69 @@
+% op_alignAllScans.m
+% Jamie Near, McGill University 2015.
+% 
+% USAGE:
+% [out,ph,frq]=op_alignAllScans(in,tmax,ref,mode);
+% 
+% DESCRIPTION:
+% Use spectral registration to align many separate scans;
+% 
+% INPUTS:
+% in        = cell array of inputs (spectra all to be registered)
+% tmax      = Maximum time (s) in time domain to use for registration.
+% ref       = Align to what? (optional)
+%                  'f' - Align to the first input?  (default)
+%                  'a' - Align to average of inputs. 
+% mode      = (optional)'f' - Frequency align only
+%                  'p' - Phase align only
+%                  'fp or pf' - Frequency and phase align (default)
+%
+
+function [out,ph,frq]=op_alignAllScans(in,tmax,ref,mode);
+
+%Make sure input is a cell array with length greater than 2;
+if ~iscell(in)  || length(in)<2
+    error('ERROR:  The input must be a cell array containing two or more MRS datasets in FID-A structure format.  ABORTING!!');
+end
+
+%Provide default values if last two arguments are not given.
+if nargin<4
+    mode='fp';
+    if nargin<3
+        ref='f';
+    end
+end
+
+%figure out what will be the reference spectrum and align spectra:
+switch ref
+    case 'f'
+        in1=in{1};
+        ampRef=max(abs(in1.fids));
+        out{1}=in{1};
+        ph(1)=0;
+        frq(1)=0;
+        
+        for n=2:length(in)
+            amp=max(abs(in{n}.fids));
+            dummyIn=op_ampScale(in{n},ampRef/amp);
+            [dummyOut,ph(n),frq(n)]=op_alignScans(dummyIn,in1,0.5);
+            out{n}=op_freqshift(op_addphase(in{n},ph(n)),frq(n));
+        end
+    case 'a'
+        in1=in{1};
+        for n=2:length(in)
+            in1=op_addScans(in1,in{n});
+        end
+        in1=op_ampScale(in1,1/length(in));
+        ampRef=max(abs(in1.fids));
+        
+        for n=1:length(in)
+            amp=max(abs(in{n}.fids));
+            dummyIn=op_ampScale(in{n},ampRef/amp);
+            [dummyOut,ph(n),frq(n)]=op_alignScans(dummyIn,in1,0.5);
+            out{n}=op_freqshift(op_addphase(in{n},ph(n)),frq(n));
+        end
+        
+    otherwise
+        error('ERROR: Reference spectrum not recognized;');
+end
+         
---
+++ processingTools/op_alignAllScans_fd.m
@@ -0,0 +1,63 @@
+% op_alignAllScans_fd.m
+% Jamie Near, McGill University 2015.
+% 
+% USAGE:
+% [out,ph,frq]=op_alignAllScans_fd(in,fmin,fmax,tmax,ref,mode);
+% 
+% DESCRIPTION:
+% Use spectral registration to align many separate scans.  In this version 
+% only a limited frequncy range is used for fitting;
+% 
+% INPUTS:
+% in        = cell array of inputs (spectra all to be registered)
+% fmin      = Minimum frequency for spectral alignment (ppm).
+% fmax      = Maximum frequency for spectral alignment (ppm).
+% tmax      = Maximum time (s) in time domain to use for registration.
+% ref       = Align to what? (optional)
+%                  'f' - Align to the first input?  (default)
+%                  'a' - Align to average of inputs. 
+% mode      = (optional)'f' - Frequency align only
+%                  'p' - Phase align only
+%                  'fp or pf' - Frequency and phase align (default)
+%
+
+function [out,ph,frq]=op_alignAllScans_fd(in,fmin,fmax,tmax,ref,mode);
+
+%Make sure input is a cell array with length greater than 2;
+if ~iscell(in)  || length(in)<2
+    error('ERROR:  The input must be a cell array containing two or more MRS datasets in FID-A structure format.  ABORTING!!');
+end
+
+%Provide default values if last two arguments are not given.
+if nargin<6
+    mode='fp';
+    if nargin<5
+        ref='f';
+    end
+end
+
+%figure out what will be the reference spectrum and align spectra:
+switch ref
+    case 'f'
+        in1=in{1};
+        out{1}=in{1};
+        ph(1)=0;
+        frq(1)=0;
+        for n=2:length(in)
+            [out{n},ph(n),frq(n)]=op_alignScans_fd(in{n},in1,fmin,fmax,0.5);
+        end
+    case 'a'
+        in1=in{1};
+        for n=2:length(in)
+            in1=op_addScans(in1,in{n});
+        end
+        in1=op_ampScale(in1,1/length(in));
+        
+        for n=1:length(in)
+            [out{n},ph(n),frq(n)]=op_alignScans_fd(in{n},in1,fmin,fmax,0.5);
+        end
+        
+    otherwise
+        error('ERROR: Reference spectrum not recognized;');
+end
+         
