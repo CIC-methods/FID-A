b577b9afbd8d5ff0ac0a57c2c1fe5d5ae4442f29
jamienear <jamie.near@mcgill.ca>
2016-01-06 11:43:00 -0500

    Made minor modification to 'op_alignAverages.m' to prevent failure when number of averages is only 1

--- processingTools/op_alignAverages.m
+++ processingTools/op_alignAverages.m
@@ -23,58 +23,69 @@ if ~in.flags.addedrcvrs
     error('ERROR:  I think it only makes sense to do this after you have combined the channels using op_addrcvrs.  ABORTING!!');
 end
 
-if nargin<4
-    parsFit=[0,0];
-else
-    parsFit=initPars;
-end
+if in.dims.averages==0
+    %DO NOTHING
+    disp('WARNING:  No averages found.  Returning input without modification!');
+    out=in;
+    fs=0;
+    phs=0;
 
-if in.dims.subSpecs==0
-    B=1;
 else
-    B=in.sz(in.dims.subSpecs);
-end
 
-fs=zeros(in.sz(in.dims.averages),B);
-phs=zeros(in.sz(in.dims.averages),B);
-fids=zeros(in.sz(in.dims.t),1,B);
-for m=1:B
-    if avg=='y' || avg=='Y'
-        disp('aligning all averages to the Average of the averages');
-        base=op_averaging(in);
-        base=[real(base.fids( in.t>=0 & in.t<tmax ,m));imag(base.fids( in.t>=0 & in.t<tmax ,m))];
-        begin=1;
+    if nargin<4
+        parsFit=[0,0];
     else
-        disp('aligning all averages to the first average');
-        base=[real(in.fids(in.t>=0 & in.t<tmax,1,m));imag(in.fids(in.t>=0 & in.t<tmax,1,m))];
-        begin=2;
-        fids(:,1,m)=in.fids(:,1,m);
+        parsFit=initPars;
     end
-    for n=begin:in.sz(in.dims.averages)
-        parsGuess=parsFit;
-        %disp(['fitting subspec number ' num2str(m) ' and average number ' num2str(n)]);
-        parsFit=nlinfit(in.fids(in.t>=0 & in.t<tmax,n,m),base,@op_freqPhaseShiftComplexNest,parsGuess);
-        fids(:,n,m)=op_freqPhaseShiftNest(parsFit,in.fids(:,n,m));
-        fs(n,m)=parsFit(1);
-        phs(n,m)=parsFit(2);
-        %plot(in.ppm,fftshift(ifft(fids(:,1,m))),in.ppm,fftshift(ifft(fids(:,n,m))));
+    
+    if in.dims.subSpecs==0
+        B=1;
+    else
+        B=in.sz(in.dims.subSpecs);
     end
+    
+    fs=zeros(in.sz(in.dims.averages),B);
+    phs=zeros(in.sz(in.dims.averages),B);
+    fids=zeros(in.sz(in.dims.t),1,B);
+    for m=1:B
+        if avg=='y' || avg=='Y'
+            disp('aligning all averages to the Average of the averages');
+            base=op_averaging(in);
+            base=[real(base.fids( in.t>=0 & in.t<tmax ,m));imag(base.fids( in.t>=0 & in.t<tmax ,m))];
+            begin=1;
+        else
+            disp('aligning all averages to the first average');
+            base=[real(in.fids(in.t>=0 & in.t<tmax,1,m));imag(in.fids(in.t>=0 & in.t<tmax,1,m))];
+            begin=2;
+            fids(:,1,m)=in.fids(:,1,m);
+        end
+        for n=begin:in.sz(in.dims.averages)
+            parsGuess=parsFit;
+            %disp(['fitting subspec number ' num2str(m) ' and average number ' num2str(n)]);
+            parsFit=nlinfit(in.fids(in.t>=0 & in.t<tmax,n,m),base,@op_freqPhaseShiftComplexNest,parsGuess);
+            fids(:,n,m)=op_freqPhaseShiftNest(parsFit,in.fids(:,n,m));
+            fs(n,m)=parsFit(1);
+            phs(n,m)=parsFit(2);
+            %plot(in.ppm,fftshift(ifft(fids(:,1,m))),in.ppm,fftshift(ifft(fids(:,n,m))));
+        end
+    end
+    
+    
+    %re-calculate Specs using fft
+    specs=fftshift(ifft(fids,[],in.dims.t),in.dims.t);
+    
+    
+    %FILLING IN DATA STRUCTURE
+    out=in;
+    out.fids=fids;
+    out.specs=specs;
+    
+    %FILLING IN THE FLAGS
+    out.flags=in.flags;
+    out.flags.writtentostruct=1;
+    out.flags.freqcorrected=1;
+    
 end
-
-
-%re-calculate Specs using fft
-specs=fftshift(ifft(fids,[],in.dims.t),in.dims.t);
-
-
-%FILLING IN DATA STRUCTURE
-out=in;
-out.fids=fids;
-out.specs=specs;
-
-%FILLING IN THE FLAGS
-out.flags=in.flags;
-out.flags.writtentostruct=1;
-out.flags.freqcorrected=1;
 
 
     function y=op_freqPhaseShiftComplexNest(pars,input)
