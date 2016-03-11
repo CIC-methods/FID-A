e067cf8a0dc11d118ff39498dc3c3f565d9ed264
jamienear <jamie.near@mcgill.ca>
2016-03-11 16:42:45 -0500

    Added a new function 'op_integrate.m' in the processing toolbox for calculating the error under the curve.

---
+++ processingTools/op_integrate.m
@@ -0,0 +1,43 @@
+% op_integrate.m
+% Jamie Near, McGill University 2016.
+% 
+% USAGE:
+% [int]=op_integrate(in,ppmmin,ppmmax,mode);
+% 
+% DESCRIPTION:
+% Basic peak integration over a specified frequency range.  By default,
+% this function integrates under the real part of the curve, but it can
+% also be made to integrate the imaginary part or the magnitude part by
+% changing the "mode" parameter.
+% 
+% INPUTS:
+% in         = input data in matlab structure format
+% ppmmin     = min of frequncy range (in ppm) in which to calculate integral.
+% ppmmax     = max of frequncy range (in ppm) in which to calculate integral.
+% mode       = mode (optional): 
+%                        -'re' (integral performed on real part).
+%                        -'im' (integral performed on imaginary part).
+%                        -'mag' (integral performed on magnitude part).
+
+function [int]=op_integrate(in,ppmmin,ppmmax,mode);
+
+if nargin<4
+    mode='re';
+    if nargin<3
+        error('ERROR:  Not enough input arguments!  ABORTING!');
+    end
+end
+
+%Now calculate the integral:
+switch mode
+    case 're'
+        int=sum(real(in.specs(in.ppm>ppmmin & in.ppm<ppmmax)),1);
+    case 'im'
+        int=sum(imag(in.specs(in.ppm>ppmmin & in.ppm<ppmmax)),1);
+    case 'mag'
+        int=sum(abs(in.specs(in.ppm>ppmmin & in.ppm<ppmmax)),1);
+    otherwise
+        error('ERROR: Mode not recognized.  Aborting!!');
+end
+
+        
