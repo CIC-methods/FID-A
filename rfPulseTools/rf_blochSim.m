33ec8dc046c81cebae7024ac5c18526e7b52aa19
jamienear <jamie.near@mcgill.ca>
2016-12-08 14:23:02 -0500

    Added a few optional input parameters to the 'rf_blochSim.m' function, including the rf pulse phase and the initial magnetization.

--- rfPulseTools/rf_blochSim.m
+++ rfPulseTools/rf_blochSim.m
@@ -2,7 +2,7 @@
 % Jamie Near, McGill University 2014.
 %
 % USAGE:
-% [mv,sc]=rf_blochSim(RF,tp,fspan,f0,peakB1);
+% [mv,sc]=rf_blochSim(RF,tp,fspan,f0,peakB1,ph,M0);
 % 
 % DESCRIPTION:
 % Perform a bloch simulation of an RF pulse.  This code simply runs Martyn 
@@ -12,25 +12,32 @@
 % INPUTS:
 % RF        = RF pulse definition structure
 % tp        = pulse duration in [ms]
-% fspan     = Frequency span in [kHz] (optional)
-% f0        = Centre of frequnecy span [kHz] (optional)
-% peakB1 	= Peak B1 amplitude in [kHz] (optional)
+% fspan     = Frequency span in [kHz] (optional.  Default=10kHz)
+% f0        = Centre of frequnecy span [kHz] (optional.  Default=0)
+% peakB1 	= Peak B1 amplitude in [kHz] (optional.  Default=RF.tw1/tp)
+% ph        = Starting phase of the rf pulse [degrees] (optional. Default=0)
+% M0        = Starting magnetization [units of M0] (optional. Default=[0,0,1])
 
 
+function [mv,sc]=rf_blochSim(RF,tp,fspan,f0,peakB1,ph,M0);
 
-function [mv,sc]=rf_blochSim(RF,tp,fspan,f0,peakB1);
-
-if nargin<5
-    peakB1=RF.tw1/tp;
-    if nargin<4
-        f0=0;
-        if nargin<3
-            fspan=10;
+if nargin<7
+    M0=[0,0,1];
+    if nargin<6
+        ph=0;
+        if nargin<5
+            peakB1=RF.tw1/tp;
+            if nargin<4
+                f0=0;
+                if nargin<3
+                    fspan=10;
+                end
+            end
         end
     end
-end        
+end
 
-[mv,sc]=bes(RF.waveform,tp,'f',peakB1,f0-fspan/2,f0+fspan/2,10000);
+[mv,sc]=bes(RF.waveform,tp,'f',peakB1,f0-fspan/2,f0+fspan/2,10000,ph,M0);
 
 figure
 subplot(3,1,1),plot(sc,mv(1,:));
