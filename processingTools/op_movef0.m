c94d990d5b6b568f3c5718cb70678956e4cee53b
jamienear <jamie.near@mcgill.ca>
2016-12-13 11:11:33 -0500

    Added a new function called 'op_movef0.m' which allows you to change the centre frequency of a spectrum.

---
+++ processingTools/op_movef0.m
@@ -0,0 +1,46 @@
+% op_movef0.m
+% Jamie Near and Eric Krochmalnek, McGill University 2016.
+% 
+% USAGE:
+% out=op_movef0(in,f);
+% 
+% DESCRIPTION:
+% Change the centre frequency of the input spectrum by 'f' Hz.
+% 
+% INPUTS:
+% in     = input data in matlab structure format.
+% f      = frequency shift to apply (in Hz).
+
+function out=op_movef0(in,f);
+
+
+% if in.dims.coils>0
+%     error('ERROR:  Can not operate on data with multilple coils!  ABORTING!!')
+% end
+% if in.dims.averages>0
+%     error('ERROR:  Can not operate on data with multiple averages!  ABORTING!!');
+% end
+% if in.dims.subSpecs>0
+%     error('ERROR:  Can not operate on data with multiple Subspecs!  ABORTING!!');
+% end
+
+t=repmat(in.t',[1 in.sz(2:end)]);
+
+fids=in.fids.*exp(-1i*t*f*2*pi);
+
+%re-calculate Specs using fft
+specs=fftshift(ifft(fids,[],in.dims.t),in.dims.t);
+
+%change the ppm scale as well
+ppm=in.ppm+(f/(in.txfrq/1000000));
+
+%plot(in1.ppm,combinedSpecs);
+
+%FILLING IN DATA STRUCTURES
+out=in;
+out.fids=fids;
+out.specs=specs;
+out.ppm=ppm;
+
+%FILLING IN THE FLAGS
+out.flags=in.flags;
