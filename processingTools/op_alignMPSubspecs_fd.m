337e54a33136efce17a2aab7e09df5da32900fe7
Jamie Near <jnear@asavpn238252.vpn.mcgill.ca>
2017-05-23 15:25:04 -0400

    Made a major modification to the 'op_alignMPSubspecs_fd.m' function to improve functionality.

--- processingTools/op_alignMPSubspecs_fd.m
+++ processingTools/op_alignMPSubspecs_fd.m
@@ -11,9 +11,11 @@
 % 
 % INPUTS:
 % in        = Input data structure.
+% minppm    = Minimum of frequency range (ppm).
+% maxppm    = Maximum of frequency range (ppm).
 % initPars	= (Optional) Initial fit parameters [freq(Hz), phase(degrees)]. Default=[0,0];
 
-function [out,fs,phs]=op_alignMPSubspecs_fd(in,initPars)
+function [out,fs,phs]=op_alignMPSubspecs_fd(in,minppm,maxppm,initPars)
 
 if ~in.flags.addedrcvrs
     error('ERROR:  I think it only makes sense to do this after you have combined the channels using op_addrcvrs.  ABORTING!!');
@@ -27,7 +29,7 @@ if in.dims.averages
    error('ERROR:  Signal averaging must be performed before this step.  ABORTING!!');
 end
 
-if nargin<3
+if nargin<4
     parsGuess=[0,0];
 else
     parsGuess=initPars;
@@ -40,41 +42,17 @@ fids=zeros(in.sz(in.dims.t),1);
 
 disp('aligning the MEGA-PRESS edit-ON sub-spectrum to the edit-OFF sub-spectrum');
 
-%NOTE:  Alignment is not performed in the regions of the spectrum where
-%the difference spectrum is large (ie.  near NAA, 3ppm GABA, or 3.7ppm
-%Glx).
-minfrq1=2.46;
-maxfrq1=2.9;
-minfrq2=3.1;
-maxfrq2=3.6;
-minfrq3=3.85;
-maxfrq3=4.1;
-minfrq4=4.6;
-maxfrq4=5.2;
-
-%freqWindow1=in.ppm>minfrq1 & in.ppm<maxfrq1; 
-%freqWindow2=in.ppm>minfrq2 & in.ppm<maxfrq2; 
-%freqWindow3=in.ppm>minfrq3 & in.ppm<maxfrq3; 
-%freqWindow4=in.ppm>minfrq4 & in.ppm<maxfrq4;
-
-%freqWindows=freqWindow1+freqWindow2+freqWindow3+freqWindow4;
-%freqWindows=logical(freqWindows);
-
-base0=op_freqrange(op_takesubspec(in,1),minfrq3,maxfrq4);
-start=op_freqrange(op_takesubspec(in,2),minfrq3,maxfrq4);
-
-%base=[real(base0.specs( freqWindows ));imag(base0.specs( freqWindows ))];
-base=[real(base0.fids);imag(base0.fids)];
+base=op_takesubspec(in,1);
+base=op_freqrange(base,minppm,maxppm);
+base=[real(base.specs);imag(base.specs)];
 begin=1;
-
-%size(in.ppm(freqWindows))
-%size(base)
-%plot([in.ppm(freqWindows) in.ppm(freqWindows)],base,'.');
+options.MaxIter=100000;
 
 %DO FITTING
 n=1;
 %disp(['fitting subspec number ' num2str(m) ' and average number ' num2str(n)]);
-parsFit=nlinfit(start.fids,base,@op_freqPhaseShiftComplexNest,parsGuess);
+datarange=op_freqrange(in,minppm,maxppm);
+parsFit=nlinfit(datarange.fids(:,2),base,@op_freqPhaseShiftComplexRangeNest,parsGuess,options);
 A=op_freqPhaseShiftNest(parsFit,in.fids(:,2));
 size(A);
 size(fids);
@@ -99,20 +77,19 @@ out.flags.writtentostruct=1;
 out.flags.freqcorrected=1;
 
 
-    function y=op_freqPhaseShiftComplexNest(pars,input)
+    function y=op_freqPhaseShiftComplexRangeNest(pars,input)
         f=pars(1);     %Frequency Shift [Hz]
         p=pars(2);     %Phase Shift [deg]
         
-        
-        dwelltime=start.dwelltime;
+        dwelltime=datarange.dwelltime;
         t=0:dwelltime:(length(input)-1)*dwelltime;
-        fid=input;
+        fid=input(:);
         
         shiftedFids=addphase(fid.*exp(-1i*t'*f*2*pi),p);
-%         shiftedSpecs=fftshift(ifft(shiftedFids,[],1),1);
-%         shiftedSpecsWindow=shiftedSpecs(ppm>minfrq3 & ppm<maxfrq4);
-        %y=real(shiftedSpecs);
-        y=[real(shiftedFids);imag(shiftedFids)];
+        shiftedSpecs=fftshift(ifft(shiftedFids,[],1),1);
+        %shiftedSpecsWindow=shiftedSpecs(freqWindows);
+        y=real(shiftedSpecs);
+        y=[real(shiftedSpecs);imag(shiftedSpecs)];
         %y=real(fid.*exp(-1i*t'*f*2*pi));
         
     end
@@ -122,8 +99,7 @@ out.flags.freqcorrected=1;
         p=pars(2);     %Phase Shift [deg]
         
         
-        dwelltime=start.dwelltime;
-        t=0:dwelltime:(length(input)-1)*dwelltime;
+        t=in.t;
         fid=input;
         p=p+180;
         
