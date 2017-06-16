2129903ab40edd2d5b6165d46af26ccbf8576c42
Jamie Near <jnear@Jamies-MacBook-Pro.local>
2017-06-15 21:55:56 -0400

    Updated both 'op_plotfid.m' and 'op_plotspec.m' to now handle datasets with more than 2 dimensions of data.  If more than 2 dimensions exist, the user is then promted to enter which dimension they would like to be displayed.  For example, if op_plotspec is used to plot a dataset containing non-singleton frequency, coils and averages dimensions, the user will be asked whether to plot all of the coil channels or all of the averages.  If the user chooses to plot all of the averages, then the all averages will be displaed for the first coil channel only.  If the user chooses to plot all of the coil channels, then all coil channels will be displayed for the first average only.

--- processingTools/op_plotfid.m
+++ processingTools/op_plotfid.m
@@ -45,8 +45,37 @@ end
 
 
 if isstruct(in)
+    if ndims(squeeze(in.fids))>2
+        disp('More than 1 dimension detected in addition to the time dimension.');
+        disp('Which dimension would you like to be displayed? ');
+        if in.dims.coils
+            disp(['     Enter: ''' num2str(in.dims.coils) ''' to display signal from all RF channels.']);
+        end
+        if in.dims.averages
+            disp(['     Enter: ''' num2str(in.dims.averages) ''' to display signal from all averages.']);
+        end
+        if in.dims.subSpecs
+            disp(['     Enter: ''' num2str(in.dims.subSpecs) ''' to display signal from all subspectra.']);
+        end
+        if in.dims.extras
+            disp(['     Enter: ''' num2str(in.dims.extras) ''' to display signal from the ''extras'' dimension.']);
+        end
+        dim=input('............ ');
+    else
+        dim=0;
+    end
     fignum=figure;
-    out=plot(in.t,real(in.fids));
+    if ~dim
+        out=plot(in.t,real(in.fids));
+    elseif dim==2
+        out=plot(in.t,real(squeeze(in.fids(:,:,1))));
+    elseif dim==3
+        out=plot(in.t,real(squeeze(in.fids(:,1,:,1))));
+    elseif dim==4
+        out=plot(in.t,real(squeeze(in.fids(:,1,1,:,1))));
+    elseif dim==5 
+        out=plot(in.t,real(squeeze(in.fids(:,1,1,1,:))));
+    end
     xlim([0 tmax]);
     set(gca,'FontSize',16);
     if isempty(ylab)
@@ -66,8 +95,35 @@ if isstruct(in)
     set(Fig1Ax1Line1, 'LineWidth', 1.2);
     %set(Fig1Ax1Line1,'MarkerSize', 10);
 elseif iscell(in)
+    if ndims(squeeze(in{1}.fids))>2
+        disp('More than 1 dimension detected in addition to the time dimension.');
+        disp('Which dimension would you like to be displayed? ');
+        if in.dims.coils
+            disp(['     Enter: ''' num2str(in{1}.dims.coils) ''' to display signal from all RF channels.']);
+        end
+        if in.dims.averages
+            disp(['     Enter: ''' num2str(in{1}.dims.averages) ''' to display signal from all averages.']);
+        end
+        if in.dims.subSpecs
+            disp(['     Enter: ''' num2str(in{1}.dims.subSpecs) ''' to display signal from all subspectra.']);
+        end
+        if in.dims.extras
+            disp(['     Enter: ''' num2str(in{1}.dims.extras) ''' to display signal from the ''extras'' dimension.']);
+        end
+        dim=input('............ ');
+    end
     figure;
-    plot(in{1}.t,real(in{1}.fids));
+    if ~dim
+        out=plot(in{1}.t,real(in{1}.fids));
+    elseif dim==2
+        out=plot(in{1}.t,real(squeeze(in{1}.fids(:,:,1))));
+    elseif dim==3
+        out=plot(in{1}.t,real(squeeze(in{1}.fids(:,1,:,1))));
+    elseif dim==4
+        out=plot(in{1}.t,real(squeeze(in{1}.fids(:,1,1,:,1))));
+    elseif dim==5 
+        out=plot(in{1}.t,real(squeeze(in{1}.fids(:,1,1,1,:))));
+    end
     ylabel('ARB UNITS');
     disp('Multiple input fids detected!! ')
     stagger=input('please enter the desired vertical spacing of the fids in ARB UNITS (0 for none):  ');
@@ -76,7 +132,17 @@ elseif iscell(in)
     hold
     colours=distinguishable_colors(length(in));
     for n=1:length(in)
-        out=plot(in{n}.t,real(in{n}.fids)+(n-1)*stagger,'Color',colours(n,:));
+        if ~dim
+            out=plot(in{n}.t,real(in{n}.fids)+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==2
+            out=plot(in{n}.t,real(squeeze(in{n}.fids(:,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==3
+            out=plot(in{n}.t,real(squeeze(in{n}.fids(:,1,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==4
+            out=plot(in{n}.t,real(squeeze(in{n}.fids(:,1,1,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==5 
+            out=plot(in{n}.t,real(squeeze(in{n}.fids(:,1,1,1,:)))+(n-1)*stagger,'Color',colours(n,:));
+        end
         xlim([0 tmax]);
         set(gca,'FontSize',16);
         if isempty(ylab)
--- processingTools/op_plotspec.m
+++ processingTools/op_plotspec.m
@@ -43,8 +43,37 @@ end
 
 
 if isstruct(in)
+    if ndims(squeeze(in.fids))>2
+        disp('More than 1 dimension detected in addition to the frequency dimension.');
+        disp('Which dimension would you like to be displayed? ');
+        if in.dims.coils
+            disp(['     Enter: ''' num2str(in.dims.coils) ''' to display signal from all RF channels.']);
+        end
+        if in.dims.averages
+            disp(['     Enter: ''' num2str(in.dims.averages) ''' to display signal from all averages.']);
+        end
+        if in.dims.subSpecs
+            disp(['     Enter: ''' num2str(in.dims.subSpecs) ''' to display signal from all subspectra.']);
+        end
+        if in.dims.extras
+            disp(['     Enter: ''' num2str(in.dims.extras) ''' to display signal from the ''extras'' dimension.']);
+        end
+        dim=input('............ ');
+    else
+        dim=0;
+    end
     fignum=figure;
-    out=plot(in.ppm,real(in.specs));
+    if ~dim
+        out=plot(in.ppm,real(in.specs));
+    elseif dim==2
+        out=plot(in.ppm,real(squeeze(in.specs(:,:,1))));
+    elseif dim==3
+        out=plot(in.ppm,real(squeeze(in.specs(:,1,:,1))));
+    elseif dim==4
+        out=plot(in.ppm,real(squeeze(in.specs(:,1,1,:,1))));
+    elseif dim==5 
+        out=plot(in.ppm,real(squeeze(in.specs(:,1,1,1,:))));
+    end
     xlim([ppmmin ppmmax]);
     set(gca,'XDir','reverse');
     set(gca,'FontSize',16);
@@ -65,8 +94,35 @@ if isstruct(in)
     set(Fig1Ax1Line1, 'LineWidth', 1.2);
     %set(Fig1Ax1Line1,'MarkerSize', 10);
 elseif iscell(in)
+    if ndims(squeeze(in{1}.fids))>2
+        disp('More than 1 dimension detected in addition to the frequency dimension.');
+        disp('Which dimension would you like to be displayed? ');
+        if in{1}.dims.coils
+            disp(['     Enter: ''' num2str(in{1}.dims.coils) ''' to display signal from all RF channels.']);
+        end
+        if in{1}.dims.averages
+            disp(['     Enter: ''' num2str(in{1}.dims.averages) ''' to display signal from all averages.']);
+        end
+        if in{1}.dims.subSpecs
+            disp(['     Enter: ''' num2str(in{1}.dims.subSpecs) ''' to display signal from all subspectra.']);
+        end
+        if in{1}.dims.extras
+            disp(['     Enter: ''' num2str(in{1}.dims.extras) ''' to display signal from the ''extras'' dimension.']);
+        end
+        dim=input('............ ');
+    end
     figure;
-    plot(in{1}.ppm,real(in{1}.specs));
+    if ~dim
+        out=plot(in{1}.ppm,real(in{1}.specs));
+    elseif dim==2
+        out=plot(in{1}.ppm,real(squeeze(in{1}.specs(:,:,1))));
+    elseif dim==3
+        out=plot(in{1}.ppm,real(squeeze(in{1}.specs(:,1,:,1))));
+    elseif dim==4
+        out=plot(in{1}.ppm,real(squeeze(in{1}.specs(:,1,1,:,1))));
+    elseif dim==5 
+        out=plot(in{1}.ppm,real(squeeze(in{1}.specs(:,1,1,1,:))));
+    end
     ylabel('ARB UNITS');
     disp('Multiple input spectra detected!! ')
     stagger=input('Please enter the desired vertical spacing of the spectra in ARB UNITS:  ');
@@ -75,7 +131,17 @@ elseif iscell(in)
     hold
     colours=distinguishable_colors(length(in));
     for n=1:length(in)
-        out=plot(in{n}.ppm,real(in{n}.specs)+(n-1)*stagger,'Color',colours(n,:));
+        if ~dim
+            out=plot(in{n}.ppm,real(in{n}.specs)+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==2
+            out=plot(in{n}.ppm,real(squeeze(in{n}.specs(:,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==3
+            out=plot(in{n}.ppm,real(squeeze(in{n}.specs(:,1,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==4
+            out=plot(in{n}.ppm,real(squeeze(in{n}.specs(:,1,1,:,1)))+(n-1)*stagger,'Color',colours(n,:));
+        elseif dim==5 
+            out=plot(in{n}.ppm,real(squeeze(in{n}.specs(:,1,1,1,:)))+(n-1)*stagger,'Color',colours(n,:));
+        end
         xlim([ppmmin ppmmax]);
         set(gca,'XDir','reverse');
         set(gca,'FontSize',16);
