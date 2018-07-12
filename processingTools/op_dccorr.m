9ecd4261bbe36067824d77981cea9673a4a0d082
Jamie Near <jnear@Jamies-MacBook-Pro.local>
2018-07-11 21:25:35 -0400

    Made a few changes to 'op_dccorr.m'.  Now, instead of filtering the data and calculating the dcoffset at a single point, the user is asked to specify a ppm range over which to calculate the dcoffset.  If the range is not specified, it is not calculated automatically using the last 0.5 ppm on each extreme end of the spectrum.  Of course, it is still possible to explicitly specify the dc offset to apply.

--- processingTools/op_dccorr.m
+++ processingTools/op_dccorr.m
@@ -2,7 +2,7 @@
 % Jamie Near, McGill University 2014.
 % 
 % USAGE:
-% out=op_dccorr(in,mode,var1);
+% [out,dcOffset]=op_dccorr(in,mode,var1);
 % 
 % DESCRIPTION:
 % Do a DC Correction on the data.  This method is a frequency domain
@@ -10,22 +10,32 @@
 % 
 % INPUTS:
 % in	= input data in matlab structure format.  
-% mode	= Point('p') or Value('v').  In point mode, the DC offset is
-%           calculated automatically at a specific point in the spectrum.  In value
-%           mode, the user has to provide the value of the desired DC offset.
-% var1	= If mode is 'p', then 'var' is the index of the spectral point that you
-%           wish to use to calculate the DC offset.  If mode is 'v', then
-%           'var' is the value of the dc offset correction that you wish to
-%           employ.
+% mode	= ppmrange('p') or Value('v').  In ppmrange mode, the DC offset is
+%           calculated automatically over a defined ppm range in the 
+%           spectrum.  In value mode, the user has to provide the value of 
+%           the desired DC offset.
+% var1	= If mode is 'p', then 'var1' is the range of ppm values 
+%           [minppm maxppm] over which to calculate the DC offset.  In 
+%           ppmrange mode, the 'var1' is optional, and takes a default 
+%           value of [max(in.ppm)-0.5 max(in.ppm] AND 
+%           [min(in.ppm) min(in.ppm)+0.5].  If mode is 'v', then 'var1' is 
+%           the value of the dc offset correction that you wish to employ.
 %
 % OUTPUTS:
 % out   = Output following DC correction.  
 
-function out=op_dccorr(in,mode,var1);
+function [out,dcOffset]=op_dccorr(in,mode,var1);
 
 if mode=='p'
-    infilt=op_filter(in,5);
-    dcOffset=infilt.specs(var1);
+    if nargin<3
+        var1a=[max(in.ppm)-0.5 max(in.ppm)];
+        var1b=[min(in.ppm) min(in.ppm)+0.5];
+        dcOffset1=mean(in.specs(in.ppm<var1a(2) & in.ppm>var1a(1)));
+        dcOffset2=mean(in.specs(in.ppm<var1b(2) & in.ppm>var1b(1)));
+        dcOffset=mean([dcOffset1 dcOffset2]);
+    else
+        dcOffset=mean(in.specs(in.ppm<var1(2) & in.ppm>var1(1)));
+    end
 elseif mode=='v'
     dcOffset=var1;
 end
