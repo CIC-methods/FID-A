a129246d6ef65c68c72e3e177c83009de73048df
Jamie Near <jnear@Jamies-MacBook-Pro.local>
2020-05-12 11:45:14 -0400

    Adding two new functions:  'sim_dMul.m' for multiplying a density matrix by a scalar, and 'sim_dDiv.m' for dividing a density matrix by a scalar.

---
+++ simulationTools/sim_dDiv.m
@@ -0,0 +1,28 @@
+%sim_dDiv.m
+%Jamie Near, 2020.
+%
+% USAGE:
+% d_out = sim_dDiv(d_in,factor)
+% 
+% DESCRIPTION:
+% Divide a density matrix by a scalar.  This function is necessary becuase
+% the density matrix is a cell array, so cannot be scaled using simple
+% division. 
+% 
+% INPUTS:
+% d_in      = input density matrix to be divided.
+% factor    = scalar factor to divide by.
+%
+% OUTPUTS:
+% d_out     = output, result of d_in/factor.
+
+function d_out = sim_dDiv(d_in,factor)
+
+d_out=cell(size(d1));
+for m=1:length(d1)
+    d_out{m}=d_in{m}./factor;
+end
+
+
+
+
