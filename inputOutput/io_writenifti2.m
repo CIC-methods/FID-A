2284b25089d3c758374c2a0eb9a9b9d1d811d89a
Peter T <truonp@outlook.com>
2023-01-31 10:14:06 -0500

    Create io_writenifti2.m
    Creating new code that takes a lot from Brenden's io_CSIwrite_nifti2.m that will also deal with SVS and CSI data.

---
+++ inputOutput/io_writenifti2.m
@@ -0,0 +1,5 @@
+function nifti=io_writenifti2(MRSIStruct,filename)
+%A lot of this code was taken from Brenden Kadota's io_CSIwrite_nifti2.m,
+%which was a work-in-progress. Goal of this code is to work with both SVS
+%and CSI data
+
