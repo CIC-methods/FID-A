aa599791fb3ca9e0c39b220b440bd63ae9633263
Peter T <truonp@outlook.com>
2023-07-13 13:38:34 -0400

    Update io_CSIwritelcm.m
    Changing to get hzppm from txfrq instead of calculating from gamma and Bo

--- inputOutput/MRSI/io_CSIwritelcm.m
+++ inputOutput/MRSI/io_CSIwritelcm.m
@@ -36,7 +36,7 @@ end
 % t0=0;
   Bo=in.Bo;
 %   hzppm=getGamma('overTwoPi', true)*Bo/1e6;
-  hzppm=in.gamma*Bo/1e6;
+  hzppm=in.txfrq/1e6;
   dwelltime=in.spectralDwellTime;
 % Nuc=0;
 % PatName='No Name';
